aae0e77671b94150a4d9b5f452b4d730e34c2f4e
Jean Sarlon <jean.sarlon@icloud.com>
2017-03-20 06:16:00 -0300

    Init

--- exercício.c
+++ exercício.c
@@ -2,5 +2,5 @@
 #include <stdio.h>
 int main() {
     #pragma omp parallel
-    printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
+    printf("Hello thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
 }
