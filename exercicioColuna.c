012b235003a5397c374237f2275389d159f8945a
Jean Sarlon <jean.sarlon@icloud.com>
2017-03-21 19:40:03 -0300

    New number

--- exercicioColuna.c
+++ exercicioColuna.c
@@ -3,19 +3,18 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define TAM 836
-int main() {
+#define TAM 2000
+int A[TAM][TAM]={0}, B[TAM][TAM]={0}, C[TAM][TAM]={0}, r1,r2;
 
-   int A[TAM][TAM]={0}, B[TAM][TAM]={0}, C[TAM][TAM]={0}, r1,r2;
+int main() {
+   omp_set_num_threads(1);
    srand(time(NULL));
 
 
    for (int i = 0; i < TAM; i++) {
       for (int j = 0; j < TAM; j++) {
-         r1 = rand()%99+1;
-         r2 = rand()%99+1;
-         A[i][j] = r1;
-         B[i][j] = r2;
+         A[i][j] = rand()%100000+1;
+         B[i][j] = rand()%100000+1;
       }
    }
 
@@ -27,23 +26,23 @@ int main() {
       }
    }
 
-   for (int i = 0; i < TAM; i++) {
-      printf("\n");
-      for (int j = 0; j < TAM; j++) {
-         if (C[i][j] > 99 && C[i][j] < 1000) {
-            printf(" %i  - ", C[i][j] );
-         }else if (C[i][j] > 9 && C[i][j] < 100) {
-            printf(" %i   - ", C[i][j] );
-         }else if (C[i][j] > 0 && C[i][j] < 10) {
-            printf(" %i    - ", C[i][j] );
-         }else{
-            printf(" %i - ", C[i][j] );
-         }
-      }
-   }
+   // for (int i = 0; i < TAM; i++) {
+   //    printf("\n");
+   //    for (int j = 0; j < TAM; j++) {
+   //       if (C[i][j] > 99 && C[i][j] < 1000) {
+   //          printf(" %i  - ", C[i][j] );
+   //       }else if (C[i][j] > 9 && C[i][j] < 100) {
+   //          printf(" %i   - ", C[i][j] );
+   //       }else if (C[i][j] > 0 && C[i][j] < 10) {
+   //          printf(" %i    - ", C[i][j] );
+   //       }else{
+   //          printf(" %i - ", C[i][j] );
+   //       }
+   //    }
+   // }
 
-   printf("\n");
-   printf("\n");
+   // printf("\n");
+   // printf("\n");
 
    //  printf("Hello %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
 }
--- exercicioMatriz.c
+++ exercicioMatriz.c
@@ -3,19 +3,22 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define TAM 836
-int main() {
 
-   int A[TAM][TAM]={0}, B[TAM][TAM]={0}, C[TAM][TAM]={0}, r1,r2;
+
+#define TAM 800
+int A[TAM][TAM]={0}, B[TAM][TAM]={0}, C[TAM][TAM]={0}, r1,r2;
+
+int main() {
+   omp_set_num_threads(300);
+
+   
    srand(time(NULL));
 
 
    for (int i = 0; i < TAM; i++) {
       for (int j = 0; j < TAM; j++) {
-         r1 = rand()%99+1;
-         r2 = rand()%99+1;
-         A[i][j] = r1;
-         B[i][j] = r2;
+         A[i][j] = rand()%999999+1;
+         B[i][j] = rand()%999999+1;
       }
    }
 
@@ -28,20 +31,20 @@ int main() {
       }
    }
 
-   for (int i = 0; i < TAM; i++) {
-      printf("\n");
-      for (int j = 0; j < TAM; j++) {
-         if (C[i][j] > 99 && C[i][j] < 1000) {
-            printf(" %i  - ", C[i][j] );
-         }else if (C[i][j] > 9 && C[i][j] < 100) {
-            printf(" %i   - ", C[i][j] );
-         }else if (C[i][j] > 0 && C[i][j] < 10) {
-            printf(" %i    - ", C[i][j] );
-         }else{
-            printf(" %i - ", C[i][j] );
-         }
-      }
-   }
+   // for (int i = 0; i < TAM; i++) {
+   //    printf("\n");
+   //    for (int j = 0; j < TAM; j++) {
+   //       if (C[i][j] > 99 && C[i][j] < 1000) {
+   //          printf(" %i  - ", C[i][j] );
+   //       }else if (C[i][j] > 9 && C[i][j] < 100) {
+   //          printf(" %i   - ", C[i][j] );
+   //       }else if (C[i][j] > 0 && C[i][j] < 10) {
+   //          printf(" %i    - ", C[i][j] );
+   //       }else{
+   //          printf(" %i - ", C[i][j] );
+   //       }
+   //    }
+   // }
 
    printf("\n");
    printf("\n");
